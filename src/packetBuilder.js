bba718713cafccf9e9a8167a438f0b695cfe4988
JP Quicksall <john.paul.quicksall@gmail.com>
2015-12-28 18:17:37 -0800

    Fixed class syntax

--- src/packetBuilder.js
+++ src/packetBuilder.js
@@ -1,19 +1,14 @@
 // The Dialog class is used to handle questions that should be asked in a dialog with the user
 
-class PacketBuilder {
-  constructor(msg, questions, url)(
+var PacketBuilder = function(msg, questions, url){
+
     this.msg = msg;
     this.user = this.msg.message.user;
     this.room = this.msg.message.room;
-    this.responses;
     this.questions = questions;
     this.responders = {};
     this.salutations = ["sweet", "cool", "awesome", "fair enough"," sounds good", "ok",
                       "fantastic", "roger that", "got it", "perfect"]
-    return init();
-  )
-
-  init(){
     this.responses = [];
     _this = this;
     for(var i = 0; i < this.questions.length; i++){
@@ -30,54 +25,53 @@ class PacketBuilder {
       this.responses.push(response);
       this.responders[single.regex] = index;
     }
-    return this;
+  )
+};
+// Clean up the used responders
+PacketBuilder.prototype.cleanUp(){
+  for(var i = this.questions.length; i >= 0; i--){
+    var index = this.responders[questions[i].regex];
+    this.msg.listeners.splice(index, 1, function(){});
+    delete this.responders[questions[i].regex];
   }
-  // Clean up the used responders
-  cleanUp(){
-    for(var i = this.questions.length; i >= 0; i--){
-      var index = this.responders[questions[i].regex];
-      this.msg.listeners.splice(index, 1, function(){});
-      delete this.responders[questions[i].regex];
-    }
-  }
+};
 
-  // Spin up the next question
-  askQuestion(num){
-    // If the num is equal to the length, time to send the packet!
-    if (num >= this.questions.length){
-      sendPacket();
-    }
-    else{
-      this.msg.send({room: this.user.name}, "" + this.responses[num].question);
-    }
+// Spin up the next question
+PacketBuilder.prototype.askQuestion(num){
+  // If the num is equal to the length, time to send the packet!
+  if (num >= this.questions.length){
+    sendPacket();
   }
+  else{
+    this.msg.send({room: this.user.name}, "" + this.responses[num].question);
+  }
+};
 
-  sendPacket(){
-    this.msg.send({
-      room: this.msg.envelope.user.name
-    }, "Making a " + guestid + " vm named " + name + " with " + memory + " megabytes of memory and " + cpu + " CPUs");
-    payload = {
-      datastore: "scaleio_vmw",
-      mem: "" + memory,
-      cpus: "" + cpu,
-      name: "" + name,
-      guestid: "" + guestid,
-      vm_version: "vmx-10",
-      user: "" + this.msg.envelope.user.name
-    };
-    this.msg.http(this.url).header('Content-Type', 'application/json').post(JSON.stringify(payload))(function(err, res, body) {
-      if (err) {
-        this.msg.logger.info("Encountered an error: " + err);
-        this.msg.send({
-          room: this.msg.envelope.user.name
-        }, "Encountered an error: " + err);
-      } else {
-        this.msg.send({
-          room: this.msg.envelope.user.name
-        }, "" + body);
-        msg.send({room: this.room}, "I have created a vm with this payload " + (JSON.stringify(payload, null, 2)));
-      }
-    });
-    cleanUp();
-  }
-}
+PacketBuilder.prototype.sendPacket(){
+  this.msg.send({
+    room: this.msg.envelope.user.name
+  }, "Making a " + guestid + " vm named " + name + " with " + memory + " megabytes of memory and " + cpu + " CPUs");
+  payload = {
+    datastore: "scaleio_vmw",
+    mem: "" + memory,
+    cpus: "" + cpu,
+    name: "" + name,
+    guestid: "" + guestid,
+    vm_version: "vmx-10",
+    user: "" + this.msg.envelope.user.name
+  };
+  this.msg.http(this.url).header('Content-Type', 'application/json').post(JSON.stringify(payload))(function(err, res, body) {
+    if (err) {
+      this.msg.logger.info("Encountered an error: " + err);
+      this.msg.send({
+        room: this.msg.envelope.user.name
+      }, "Encountered an error: " + err);
+    } else {
+      this.msg.send({
+        room: this.msg.envelope.user.name
+      }, "" + body);
+      msg.send({room: this.room}, "I have created a vm with this payload " + (JSON.stringify(payload, null, 2)));
+    }
+  });
+  cleanUp();
+};

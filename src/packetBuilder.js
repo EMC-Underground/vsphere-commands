ce1a02eb2eeba66eebad8f794969ff9afe77c07a
JP Quicksall <john.paul.quicksall@gmail.com>
2015-12-28 18:21:51 -0800

    Fixed tabs

--- src/packetBuilder.js
+++ src/packetBuilder.js
@@ -1,31 +1,29 @@
 // The Dialog class is used to handle questions that should be asked in a dialog with the user
 
 var PacketBuilder = function(msg, questions, url){
-
-    this.msg = msg;
-    this.user = this.msg.message.user;
-    this.room = this.msg.message.room;
-    this.questions = questions;
-    this.responders = {};
-    this.salutations = ["sweet", "cool", "awesome", "fair enough"," sounds good", "ok",
-                      "fantastic", "roger that", "got it", "perfect"]
-    this.responses = [];
-    _this = this;
-    for(var i = 0; i < this.questions.length; i++){
-      var single = this.questions[i];
-      var response = {'key': '', 'question': '', 'answer': ''};
-      response.key = single.dataname;
-      response.question = single.question;
-      msg.respond(single.regex, function(msg, i, _this){
-        _this.responses[i].answer = msg.match[2];
-        msg.send(_this.salutations[Math.floor(Math.random()* responses.length)]);
-        _this.askQuestion(i+1);
-      });
-      var index = this.msg.listeners.length - 1;
-      this.responses.push(response);
-      this.responders[single.regex] = index;
-    }
-  )
+  this.msg = msg;
+  this.user = this.msg.message.user;
+  this.room = this.msg.message.room;
+  this.questions = questions;
+  this.responders = {};
+  this.salutations = ["sweet", "cool", "awesome", "fair enough"," sounds good", "ok",
+                    "fantastic", "roger that", "got it", "perfect"]
+  this.responses = [];
+  _this = this;
+  for(var i = 0; i < this.questions.length; i++){
+    var single = this.questions[i];
+    var response = {'key': '', 'question': '', 'answer': ''};
+    response.key = single.dataname;
+    response.question = single.question;
+    msg.respond(single.regex, function(msg, i, _this){
+      _this.responses[i].answer = msg.match[2];
+      msg.send(_this.salutations[Math.floor(Math.random()* responses.length)]);
+      _this.askQuestion(i+1);
+    });
+    var index = this.msg.listeners.length - 1;
+    this.responses.push(response);
+    this.responders[single.regex] = index;
+  }
 };
 // Clean up the used responders
 PacketBuilder.prototype.cleanUp(){
